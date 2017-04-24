7b67cdd374a0872f0717dfe5fe424afdb6c6af88
ElDainosor <eldainosor@gmail.com>
2017-04-24 11:03:19 -0300

    Testing with WebP fallback + Material by Daemonite bringup

--- shishu/js/script.js
+++ shishu/js/script.js
@@ -1,4 +1,4 @@
-var roms = ['AICP','RR','AOSiP', 'DU','CrDroid','Lineage','XenonHD','More'];
+var roms = ['AICP','RR','AOSiP', 'DU','CrDroid','Lineage','XenonHD','AOKP','Darkness Redefined','AquariOS','Purenexus','OmniROM','AOSP Extended'];
 
 /*Thx stackoverflow*/
 
